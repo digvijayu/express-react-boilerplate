123884ca80720e5da0845d29365c5de65b32f393
Digvijay Upadhyay <digvijay.upadhyay@globant.com>
2018-12-05 14:03:16 +0000

    fixed unit tests

--- server/tests/app.test.js
+++ server/tests/app.test.js
@@ -6,11 +6,9 @@ const app = require('./../app.js');
 test('should return expected status when home page is accessed', async t => {
   const response = await request(app).get('/');
   t.is(response.status, 200);
-  t.deepEqual(response.status, 200);
 });
 
 test('should return json response for api', async t => {
   const response = await request(app).get('/api');
   t.is(response.status, 200);
-  t.deepEqual(response.status, 200);
 });
